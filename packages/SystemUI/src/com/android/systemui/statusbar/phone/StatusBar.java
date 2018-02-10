889056d84309f48e6b2c3b17c99d9ebd15e9c8ec
Ivan Iskandar <ivan@prjkt.io>
2018-02-11 02:12:50 +0700

    [WIP] SystemUI: Reload more assets on overlay change
    This commit mainly attempt to complete the current feature of
    overlay change assets reloads implemented with https://github.com/SubstratumResources/platform_frameworks_base/commit/987f1937a0231a2b6d09bf7c3bbb7df9de04d6b4
    which miss the notification and other small bits in SystemUI to
    be reloaded.
    
    If done correctly, it will eliminate the need of SystemUI
    restart when changing framework or SystemUI overlay.
    
    Change-Id: I451be5e6c65bf938706ae2a77aad1cef653c5ba5
    Signed-off-by: Ivan Iskandar <ivan@prjkt.io>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1482,6 +1482,15 @@ public class StatusBar extends SystemUI implements DemoMode,
         reevaluateStyles();
     }
 
+    @Override
+    public void onOverlayChanged() {
+        updateNotificationsOnOverlayChanged();
+        mStackScroller.onOverlayChanged();
+        mNotificationShelf.onOverlayChanged();
+        mNotificationPanel.onOverlayChanged();
+        Dependency.get(DarkIconDispatcher.class).onOverlayChanged(mContext);
+    }
+
     private void reinflateViews() {
         reevaluateStyles();
 
@@ -1525,6 +1534,20 @@ public class StatusBar extends SystemUI implements DemoMode,
             boolean exposedGuts = mNotificationGutsExposed != null
                     && entry.row.getGuts() == mNotificationGutsExposed;
             entry.row.onDensityOrFontScaleChanged();
+            if (exposedGuts) {
+                mNotificationGutsExposed = entry.row.getGuts();
+                bindGuts(entry.row, mGutsMenuItem);
+            }
+        }
+    }
+
+    private void updateNotificationsOnOverlayChanged() {
+        ArrayList<Entry> activeNotifications = mNotificationData.getActiveNotifications();
+        for (int i = 0; i < activeNotifications.size(); i++) {
+            Entry entry = activeNotifications.get(i);
+            boolean exposedGuts = mNotificationGutsExposed != null
+                    && entry.row.getGuts() == mNotificationGutsExposed;
+            entry.row.onOverlayChanged();
             if (exposedGuts) {
                 mNotificationGutsExposed = entry.row.getGuts();
                 bindGuts(entry.row, mGutsMenuItem);
